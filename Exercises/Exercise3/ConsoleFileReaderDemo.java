e86074640d568cf00f8d103e298a8fec0ce41f10
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-12-31 02:39:38 +0530

    Add ConsoleFileReaderDemo.java

---
+++ Exercises/Exercise3/ConsoleFileReaderDemo.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class ConsoleFileReaderDemo {
+    public static void main(String[] args) throws IOException {
+        ConsoleFileReader cfr = new ConsoleFileReader();
+        cfr.readFromFile();
+    }
+}
